f458268a09044c119fd69b3ca1008668dd6d3c46
Максим Дмитриевич Мальцев <MaltsevMD@stud.rosnou.ru>
2022-12-29 12:00:59 +0300

    345

--- CourseWork/ticket.h
+++ CourseWork/ticket.h
@@ -2,47 +2,42 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <algorithm>
 #include <fstream>
 #include <random>
-#include <fcntl.h>
-#include <io.h>
-#include <stringapiset.h>
 
-std::wstring utf8_decode(const std::wstring& instr) noexcept;
+std::wstring utf8_decode(const std::wstring& instr) noexcept; //используется для перевода из кодировки UTF-8
+std::string unicode2ansi(const std::wstring& wstr) noexcept; //используется для перевода в кодировку ANSI
+//2 функции, необходимые для вывода в файл .txt, учитывая использование std::wstring
 
-std::string unicode2ansi(const std::wstring& wstr) noexcept;
-
-bool operator==(std::vector <std::wstring> a, std::vector <std::wstring> b) noexcept;
+bool operator==(std::vector <std::wstring> a, std::vector <std::wstring> b) noexcept; //перегрузка сравения wstring векторов, необходима для поиска одинаковых билетов
 
 class ticket {
-    std::vector <std::vector <std::wstring>> vars; //Список задач [["abc", "vrd"], ["f3f", "wefw"], [] ...]
-    std::vector <int> tasks_per_module_quantity; //Список количества задач для вывода
-    int variants_quantity = 0; //Количество вариантов
-    int max_variants_quantity = 0;
-    char output_mode[1]{ 't' }; //Режим вывода
+    std::vector <std::vector <std::wstring>> vars; //Список необработанных задач [["abc", "vrd"], ["f3f", "wefw"], [] ...], задается при считвании файла
+    std::vector <int> tasks_per_module_quantity; //Список количества задач для вывода из каждого модуля, задается при считвании файла
+    int variants_quantity = 0; //Количество вариантов, задается в интерфейсе
+    int max_variants_quantity = 0; //Количество максимальных вариантов, высчитывается методом calcualte_max_variants_quantity()
+    char output_mode[1]{ 't' }; //Режим вывода, задается в интерфейсе
 public:
-    ~ticket() noexcept = default;
+    ~ticket() noexcept = default; //Стандартный деконструктор
 
-    void clear() noexcept;
+    void clear() noexcept; //Очистка данных, необходим для "сброса" программы
 
-    void calcualte_max_variants_quantity() noexcept;
+    void calcualte_max_variants_quantity() noexcept; //Вызывается при считывании файла задач, считает максимальное количество уникальных сочетаний, по сути программная реализация уравнения комбинаторики
 
-    int max_variants() noexcept;
+    int max_variants() noexcept; //Вывод количества максимальных вариантов, необходим для работы интерфейса
 
-    void variants(const int& variants) noexcept;
+    void variants(const int& variants) noexcept; //Ввод количества необходимых вариантов, передается объектом класса интерфейса
 
-    void fprint_mode(const char& mode) noexcept;
+    void fprint_mode(const char& mode) noexcept; //Ввод режима вывода файла, передается объектом класса интерфейса
 
-    void fprint(const std::vector <std::vector <std::wstring>>& vec);
+    void fprint(const std::vector <std::vector <std::wstring>>& vec); //Вывод в файл, вызвается методом random_fprint() в конце его работы, он передает в метод ссылку на форматированную под вывод векторную матрицу строк.
 
-    short int scan(const std::string input_path);
+    short int scan(const std::string input_path); //Считвание файла в vars, парралельно использует метод module(const std::wstring& str) для поиска модулей. Записывает количество выводимых из этих модулей задач в tasks_per_module_quantity.
 
-    int module(const std::wstring& str);
+    int module(const std::wstring& str); //Поиск правильно записанных команд - Module *цифры*
 
-    int size() noexcept;
+    int size() noexcept; //Вывод размера vars, необходимо для работы интерфейса
 
-    void random_fprint() noexcept;
+    void random_fprint() noexcept; //Основной алгоритм построения билетов (вариантов), создает случайные варианты и смотрит являются ли они уникальными. Если нет, генерирует заново.
 
 };
-extern ticket processing_class;
